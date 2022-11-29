8f694add7eaf09254f1624df2e8846a42cb50f8c
Gravemind1142 <33325841+Gravemind1142@users.noreply.github.com>
2022-11-29 15:56:33 -0800

    added sensor, motor test to main

--- main.c
+++ main.c
@@ -18,6 +18,11 @@
 #include <stdlib.h>
 
 #include "src/sensor.h"
+#include "src/External_Libraries/MotorDriver.h"
+
+// GPIO Pin Numbers
+#define REFL_L 20 // Reflective sensor left
+#define REFL_R 21 // Reflective sensor right
 
 // Flag to tell loops when to stop
 int running;
@@ -34,13 +39,43 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
+	// I2C, PWM, MOTOR HAT Init
+	Motor_Init();
+
+	// Left Reflective Sensor Init
+	// Pointer for input init
+	int reflLeftInput = 0;
+	int* reflLeftInputPtr = &reflLeftInput;
+	setupSimpleSensor(REFL_L, reflLeftInputPtr); // setup thread
+
+	// Right Reflective Sensor Init
+	// Pointer for input init
+	int reflRightInput = 0;
+	int* reflRightInputPtr = &reflRightInput;
+	setupSimpleSensor(REFL_R, reflRightInputPtr); // setup thread
+
 	// flag init
 	running = 1;
 
 	signal(SIGINT, sigint);
 
+	// BUTTON SIGNAL GOES HERE
+
 	while (running) {
 		// decision handling here
+
+		// Sensor and Motor test
+		if (*reflLeftInputPtr == 1) {
+			Motor_Run(MOTORA, FORWARD, 100);
+		} else {
+			Motor_Run(MOTORA, FORWARD, 0);
+		}
+
+		if (*reflRightInputPtr == 1) {
+			Motor_Run(MOTORB, FORWARD, 100);
+		} else {
+			Motor_Run(MOTORB, FORWARD, 0);
+		}
 
 		// break and set running = false when done
 	}
