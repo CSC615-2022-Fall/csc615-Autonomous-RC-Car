cea522d2308e56a373da0e3827ce4a983fbd307a
Gravemind1142 <sunicorafael@yahoo.co.uk>
2022-12-12 16:01:16 -0800

    fixed infinite loop bug

--- src/ThreadHandler/ThreadSensor.c
+++ src/ThreadHandler/ThreadSensor.c
@@ -25,12 +25,12 @@ void* thread_collect_echo_sensor_data(void* threadArgs)
         }
 
         // keep setting start timer until ECHO doesn't read 0
-        while (gpioRead(*echoPin) == 0) {
+        while (*isRunning == RUN_ON && gpioRead(*echoPin) == 0) {
             clock_gettime(CLOCK_REALTIME, &startTime);
         }
 
         // keep setting end timer until ECHO doesn't read 1
-        while (gpioRead(*echoPin) == 1) {
+        while (*isRunning == RUN_ON && gpioRead(*echoPin) == 1) {
             clock_gettime(CLOCK_REALTIME, &endTime);
         }
 
