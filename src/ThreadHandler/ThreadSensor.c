b2c54f6a27d7ba5a8aff1d5514d5d59af5bf76c2
Gravemind1142 <sunicorafael@yahoo.co.uk>
2022-12-13 20:28:15 -0800

    Revert "s"
    This reverts commit 0dc4312b8d855f463a5ef27ade9eea4a45056faa.

--- src/ThreadHandler/ThreadSensor.c
+++ src/ThreadHandler/ThreadSensor.c
@@ -24,26 +24,14 @@ void *thread_collect_echo_sensor_data(void *threadArgs) {
       pthread_exit(NULL);
     }
 
-    if (*echoPin == 13) {
-      printf("yo");
-    }
-
     // keep setting start timer until ECHO doesn't read 0
     while (*isRunning == RUN_ON && gpioRead(*echoPin) == 0) {
       clock_gettime(CLOCK_REALTIME, &startTime);
     }
 
-    if (*echoPin == 13) {
-      printf("1");
-    }
-
     // keep setting end timer until ECHO doesn't read 1
     while (*isRunning == RUN_ON && gpioRead(*echoPin) == 1) {
       clock_gettime(CLOCK_REALTIME, &endTime);
-    }
-
-    if (*echoPin == 13) {
-      printf("2\n");
     }
 
     // calulcate elapsed time
@@ -58,9 +46,9 @@ void *thread_collect_echo_sensor_data(void *threadArgs) {
     // load distance into int* input
     sensor->data = (int)distance;
 
-    //if (*echoPin == 13) {
-    //  printf("%d %d\n",*echoPin,sensor->data);
-    //}
+    if (*echoPin == 13) {
+      printf("%d %d\n",*echoPin,sensor->data);
+    }
 
     usleep(100);
   }
