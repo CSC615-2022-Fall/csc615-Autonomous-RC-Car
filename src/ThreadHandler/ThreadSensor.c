f92128229d2e57a341af11e906dcdc4e21d0be3e
Gravemind1142 <sunicorafael@yahoo.co.uk>
2022-12-13 18:46:22 -0800

    Revert "print test"
    This reverts commit 947a38f12dffb257776ab3e01edde0d370399588.

--- src/ThreadHandler/ThreadSensor.c
+++ src/ThreadHandler/ThreadSensor.c
@@ -79,7 +79,7 @@ void *thread_collect_line_sensor_data(void *threadArgs) {
   while (*isRunning == RUN_ON) {
     sensor->data = gpioRead(pin);
 
-    usleep(100);
+    usleep(20);
   }
   pthread_exit(NULL);
 }
